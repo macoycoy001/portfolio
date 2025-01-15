da70affc691578013da798b4b4c6d1d2dfe00342
macoycoy001 <marcomalaluan86@gmail.com>
2025-01-15 08:19:21 +0800

    initial commit

---
+++ marco.py
@@ -0,0 +1,12 @@
+
+last_name = "malaluan"
+
+# bbbb
+fullname = "MARCO" + " " + last_name
+
+# name = input("What is your first name?")
+# middlename = input("What is my middle name?")
+# nickname = input("what is my nickname?")
+# print("Hello " + name + " " + middlename)
+# print("Can i call you " + nickname)
+print(fullname)
